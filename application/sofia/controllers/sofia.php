688cc025a2f6ba0c0911eb41238f20850d4235da
afinedayproductions <afinedayproductions@gmail.com>
2014-05-10 13:08:23 +0200

    Add languages to front-page + delete profile page

--- application/sofia/controllers/sofia.php
+++ application/sofia/controllers/sofia.php
@@ -6,6 +6,9 @@ class Sofia extends MY_Controller {
 		parent::__construct();
 		$this->load->library('layout');
 		$this->layout->add_js('nav-fixed');
+
+		//! Load models
+		$this->load->model('m_works');
 	}
 
 
@@ -13,23 +16,66 @@ class Sofia extends MY_Controller {
 	//! Main app page
 	public function index() {
 
-		// Get work resume
-		$this->load->model('m_works');
+		
+
+		/*** Récupération des 3 derniers travaux ***/
+
 		$works = $this->m_works->getLastWorksResume(3);
+
+		/*** end Récupération des 3 derniers travaux ***/
+
+
+
+		/*** Récupération des données sur les langages ***/
+
+		$langages = $this->m_works->getLanguages();
+		$today = explode('-', date('Y-m-d'));
+
+		foreach ($langages as $key => $langage) {
+
+			$date = explode('-', $langage->duration);
+
+			$duration_year = $today[0] - $date[0];
+			$duration_month = $today[1] - $date[1];
+
+			// month fix
+			if($duration_month < 0)
+				$duration_month += 12; // Add 12 months if month is negative
+
+			// Date assignation
+			if($duration_year == 0)
+				$langage->duration = $duration_month . ' mois';
+			else
+				$langage->duration = $duration_year . ' ans';
+
+		} // end foreach $langages
+
+		$langages_column_separator = ceil((count($langages) / 3));
+
+		/*** end  Récupération des données sur les langages ***/
+
+
+
+		/*** SET CI DATA ***/
+
+		$data['langages'] 					= $langages;
+		$data['langages_column_separator'] 	= $langages_column_separator;
 		$data['works'] = $works;
+
+		/*** END SET CI DATA ***/
 
 
 
 		/*** Define view ***/
 
 		// Page title
-		$this->layout->set_second_part_title('Développeur PHP - web à Toulouse');
+		$this->layout->set_second_part_title('Développeur PHP / web à Toulouse');
 
 		// views
 		$this->layout->views('v_header');
 		$this->layout->views('v_nav');
 		$this->layout->views('v_works', $data);
-		$this->layout->views('v_profil');
+		$this->layout->views('v_profil', $data);
 		$this->layout->views('v_savoir-faire');
 		$this->layout->view('v_footer');
 		
--- application/sofia/views/v_profil.php
+++ application/sofia/views/v_profil.php
@@ -3,15 +3,48 @@
 	<h2>Qui suis-je ?</h2>
 	<h3>Et ce que je veux</h3>
 
-	<p>
-		Je suis Fabien MATHIS, j'ai 23 ans. Je suis un développeur web à Toulouse, France. Je viens de terminer une licence pro RTAI à l'université Toulouse 1 Capitole.
-		<br><br>
-		Je recherche actuellement une agence web ou un agence de communication à Toulouse ou en proche banlieue pour un travail ! Si vous avez besoin d'un développeur web, vous pouvez télécharger mon CV.
-	</p>
+	<div class="langages-group clearfix">
 
-	<div class="a-block">
-		<a class="a-block" href="<?php echo docs_url('Fabien_MATHIS_CV.pdf'); ?>" download="Fabien_MATHIS_CV.pdf">Télécharger mon CV</a>
+		<div>Langages:</div>
+
+		<div class="langages">
+		
+		<?php for ($i = 0; $i < count($langages); $i++) {
+
+			if($i == $langages_column_separator || $i == 2 * $langages_column_separator)
+				echo '</div>
+		<div class="langages">
+			<div>' . $langages[$i]->name . ' <span>' . $langages[$i]->duration . '</span></div>';
+			else
+				echo '<div>' . $langages[$i]->name . ' <span>' . $langages[$i]->duration . '</span></div>';
+		
+		} ?>
+
+		</div>
+
 	</div>
+	<!-- end .clearfix -->
+
+	<div class="un-petit-mot">
+
+		<div>Un petit mot:</div>
+
+		<br>
+
+		<p>
+			Je suis Fabien MATHIS, j'ai 23 ans. Je suis un développeur web à Toulouse, France. 
+			<br>
+			J'ai validé une licence pro RTAI à l'université Toulouse 1 Capitole.
+			<br><br>
+			Je recherche actuellement une agence web ou un agence de communication à Toulouse ou en proche banlieue pour un travail ! Si vous avez besoin d'un développeur web, vous pouvez télécharger mon CV.
+		</p>
+
+		<div class="a-block">
+			<a class="a-block" href="<?php echo docs_url('Fabien_MATHIS_CV.pdf'); ?>" download="Fabien_MATHIS_CV.pdf">Télécharger mon CV</a>
+		</div>
+
+	</div>
+	<!-- end .un-petit-mot -->
 
 </div>
 <!-- end #profil-index -->
